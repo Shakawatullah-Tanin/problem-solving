03ddbbd9492d0cc067941c998a5e2924ad07513d
Shakawatullah-Tanin <shakawatullahtanin23@gmail.com>
2025-02-19 15:54:03 +0600

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function calculateVAT(price){
-    if(price <0 || isNaN(price)){
+function calculateVAT(price) {
+    if (price < 0 || isNaN(price) || typeof (price) != 'number') {
         return "invalid";
     }
-    else{
-        let vat= price*7.5/100;
-        return vat +' ' +"Taka";
+    else {
+        let vat = price * 7.5 / 100;
+        return vat + ' ' + "Taka";
     }
 }
 
@@ -12,14 +12,14 @@ function calculateVAT(price){
 function validContact(contact) {
     let number = contact.toString();
     let firsttNumber = number.slice(0, 2)
-    if(typeof(contact) !== 'string'){
+    if (typeof (contact) !== 'string') {
         return false;
     }
-    if (number.length === 11) {
+    else if (number.length === 11) {
         if (firsttNumber === '01') {
             if (number.includes(' ')) {
                 return false;
-            }else{
+            } else {
                 return true;
             }
         } else {
@@ -28,43 +28,64 @@ function validContact(contact) {
         }
     } else {
         return false;
-    }
+    }
 }
 
 
-function willSuccess(marks){
-    let count =0 ;
-    for(let num =0; num<marks.length;num++){
-        if (typeof marks[num]!== "number") {
+function willSuccess(marks) {
+    let count = 0;
+    for (let num = 0; num < marks.length; num++) {
+        if (typeof marks[num] !== "number") {
             return "invalid";
         }
-        if(marks[num]>50){
+        else if (marks[num] >= 50) {
             count++;
         }
     }
-    if (count<marks.length / 2) {
+    if (count > marks.length / 2) {
         return true;
     } else {
         return false;
     }
 }
-// const marks=willSuccess([50,60,70,40,30,]);
-// console.log(marks);
 
 
-function validProposal(person1,person2){
-    if(Math.abs(person1.age-person2.age)>7){
+
+function validProposal(person1, person2) {
+    if (typeof (person1) != "object" || typeof (person2) != "object") {
+        return "invalid";
+    }
+    else if (Math.abs(person1.age - person2.age) > 7) {
         return false;
     }
-    if(person1.gender===person2.gender){
+    else if (person1.gender === person2.gender) {
         return false;
     }
-    else{
+    else {
         return true;
     }
 }
-const people1={name:"alam",age:20,gender:"male"};
-const people2={name:"asika",age:14,gender:"female"};
-// const people2=["taanon", 20, false];
-const person = validProposal(people1,people2);
-console.log(person);
+
+
+function calculateSleepTime(times) {
+    if (!Array.isArray) {
+        return "invalid"
+    }
+    else {
+        let totalSecond = 0;
+        for (let i = 0; i < times.length; i++) {
+            if (typeof (times[i]) == "number") {
+                totalSecond = totalSecond + times[i]
+            }
+            else {
+                return "invalid";
+            }
+        }
+        let hour = Math.floor(totalSecond / 3600);
+        totalSecond %= 3600;
+        let minute = Math.floor(totalSecond / 60);
+        let second = totalSecond % 60;
+        let sleep={hour,minute,second}
+        return sleep;
+    }
+}
